9297539d6527a5d1b04c32090cac7e4ffe441937
jgoodall628 <jgoodall628@gmail.com>
2014-10-20 22:22:54 -0500

    Example 34

---
+++ ex34.rb
@@ -0,0 +1,19 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+puts animals[1]
+puts animals[2]
+puts animals[0]
+puts animals[3]
+puts animals[4]
+puts animals[2]
+puts animals[5]
+puts animals[3]
+
+# 1. ruby
+# 2. peacock
+# 3. bear
+# 4. kangaroo
+# 5. whale
+# 6. peacock
+# 7. platypus
+# 8. whale
